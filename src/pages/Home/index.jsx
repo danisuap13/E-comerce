accd9773a5e789f627b5c094c1e223c5fd957f05
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-08-02 21:40:06 -0500

    Navbar, Layout y Cards estructura y diseño

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+import Card from "../../components/Card";
+
+function Home() {
+  return (
+		<Layout >
+      Home
+			<Card />
+		</Layout>
+	);
+};
+
+export default Home;
