2cd9ee2942ec28050f4f4487a28fd318ae40e3f0
Daniel Suarez <est.daniel.suarez1@unimilitar.edu.co>
2023-08-09 12:18:57 -0500

    Se agrega funcionalidad checkout

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -9,19 +9,19 @@ const OrderCard = (props) => {
 		context.setProductToShow(productDetail)
 	}
 	
-	const { id, title, image , price, value, description} = props;
+	const { id, title, image , price, value, description, handleDelete} = props;
 	return(
 		<div className='flex justify-between items-center mb-3 border border-gray-300 rounded-lg'>
 				<p className='mx-1 text-md font-light border border-black rounded-lg p-2 hover:cursor-pointer hover:border-white' onClick={() => context.updateCardValue(id)} >{value}</p>
 			<div className='flex items-center gap-2'>
 				<figure className="w-20 h-20">
-					<img onClick={()=>showProduct({id, title, image, price, description})} className="w-full h-full rounded-lg object-cover hover:cursor-pointer" src={image} alt={title} />
+					<img /* onClick={()=>showProduct({id, title, image, price, description})} */ className="w-full h-full rounded-lg object-cover hover:cursor-pointer" src={image} alt={title} />
 				</figure>
 				<p className='text-sm font-light'>{title.slice(0,20)}...</p>
 			</div>
 			<div className='flex items-center gap-1'>
 				<p className="text-lg font-medium ml-2">${(price*value).toFixed(2)}</p>
-				<TrashIcon className='h-6 w-6 text-black mr-1 hover:cursor-pointer' onClick={() => context.handleDelete(id, value)}/>
+				<TrashIcon className='h-6 w-6 text-black mr-1 hover:cursor-pointer' onClick={() => handleDelete(id, value)}/>
 			</div>
 		</div>
 	);
